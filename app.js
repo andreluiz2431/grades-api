4df807cc9a8b7add20abc8ab3783a637e33e67e3
André Luiz <alm28062001@gmail.com>
2020-07-11 23:26:08 -0300

    ahhh

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     // endereço do front
-    origin: 'https://grade-front-app.herokuapp.com',
+    origin: 'https://grade-front-app.herokuapp.com/',
   })
 );
 
